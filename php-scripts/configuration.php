09caa3e4b3583122fecd459f62e2c4e6a9b88ce9
Juanes Roig <roitxo11@gmail.com>
2020-01-17 16:12:32 +0100

    Database config
    database config

--- php-scripts/configuration.php
+++ php-scripts/configuration.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST','localhost');
-define('DB_DB','mydreamteam');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','juanesroig.com');
+define('DB_DB','u840799087_mydreamteam');
+define('DB_USER','u840799087_my_dream_team');
+define('DB_PASS','3>=,gFmm9Y`,=&22');

b4ff17f995983bf830ec7de2a494405410f7e2be
Juanes Roig <roitxo11@gmail.com>
2020-01-23 16:39:45 +0100

    V2

---
+++ sql/insert_playersv2.sql
@@ -0,0 +1,31 @@
+--DELANTEROS
+INSERT INTO jugadores
+    (Nombre, Media, Imagen, Pos)
+VALUES
+    ('Kylian Mbappe', 89, 'img/delanteros/Mbappe.jpg', 'delantero'),
+    ('Marco Van Basten', 93, 'img/delanteros/VanBasten.jpg', 'delantero'),
+    ('Heung Min Son', 87, 'img/delanteros/Son.jpg', 'delantero'),
+    ('Rodrigo Moreno', 83, 'img/delanteros/Rodrigo.jpg', 'delantero'),
+    ('Joao Felix', 80, 'img/delanteros/JoaoFelix.jpg', 'delantero'),
+    ('Ante Budimir', 72, 'img/delanteros/Budimir.jpg', 'delantero')
+
+
+--CENTROCAMPISTAS
+INSERT INTO jugadores
+    (Nombre, Media, Imagen, Pos)
+VALUES
+    ('Ryan Giggs', 92, 'img/centrocampistas/Giggs.jpg', 'centrocampista'),
+    ('Paulo Dybala', 88, 'img/centrocampistas/Dybala.jpg', 'centrocampista'),
+    ('Jadon Sancho', 84, 'img/centrocampistas/Sancho.jpg', 'centrocampista'),
+    ('Samu Castillejo', 80, 'img/centrocampistas/Castillejo.jpg', 'centrocampista'),
+    ('Ruben Herreros', 99, 'img/centrocampistas/Ruben.jpg', 'centrocampista')
+
+
+--DEFENSAS
+INSERT INTO jugadores
+    (Nombre, Media, Imagen, Pos)
+VALUES
+    ('Javier Zanetti', 92, 'img/defensas/Zanetti.jpg', 'defensa'),
+    ('Aymeric Laporte', 87, 'img/defensas/Laporte.jpg', 'defensa'),
+    ('Sokratis', 84, 'img/defensas/Sokratis.jpg', 'defensa'),
+    ('Domenico Criscito', 79, 'img/defensas/Criscito.jpg', 'defensa')
